5ecd2117e4ae2f7caab9291a091a83156d094354
Mauricio kruger <mau_kriger@hotmail.com>
2023-06-09 13:07:43 -0300

    feat(tarefa): criado componente

---
+++ src/components/ATarefa.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">
+        Descrição da tarefa
+      </div>
+      <div class="column ">
+        <OCronometro :tempoEmSegundos="15" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import OCronometro from './OCronometro.vue'
+export default defineComponent ({
+  name: 'ATarefa',
+  components:  { OCronometro },
+  data () {
+    return {
+    }
+  },
+  methods: {}
+})
+</script>
+
+<style scoped>
+.box {background: #FAF0CA;}
+</style>

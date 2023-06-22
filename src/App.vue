73fcaa3ea5c8df3f214998889b0f119cd81e66cf
Mauricio kruger <mau_kriger@hotmail.com>
2023-06-22 20:45:07 -0300

    feat(tracker): adicionado verificacao de tarefas vazias

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
       <OFormulario @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
         <ATarefa :tarefa="tarefa" v-for="(tarefa, index) in tarefas" :key="index" />
+        <OAviso v-if="listaEstaVazia">
+          Você não está muito produtivo hoje :(
+        </OAviso>
       </div>
     </div>
   </main>
@@ -16,17 +19,24 @@
 import BarraLateral from './components/BarraLateral.vue'
 import OFormulario from './components/OFormulario.vue'
 import ATarefa from './components/ATarefa.vue'
+import OAviso from './components/OAviso.vue'
 
 export default {
   name: "App",
   components: { 
     BarraLateral,
     OFormulario,
-    ATarefa
+    ATarefa,
+    OAviso
   },
   data () {
     return {
       tarefas: []
+    }
+  },
+  computed: {
+    listaEstaVazia () {
+      return this.tarefas.length === 0
     }
   },
   methods: {
